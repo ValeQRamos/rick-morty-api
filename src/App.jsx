6a595d27bdcf04b01f5def412127c0136e38dd4c
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-05-19 16:24:53 -0500

    adding all characters

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./views/Homepage/Homepage";
 import About from "./views/About/About";
+import { ApiContextProvider } from "./context/ApiContext";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <ApiContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </ApiContextProvider>
     </>
   );
 }
--- src/views/Homepage/Homepage.jsx
+++ src/views/Homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import Characters from "../../components/Characters/Characters";
 
 const Homepage = () => {
-  return <div>
-    <h1>All Characters</h1>
-    <Characters />
-  </div>
+  return (
+    <div>
+      <h1>All Characters</h1>
+      <Characters />
+    </div>
+  );
 };
 export default Homepage;
